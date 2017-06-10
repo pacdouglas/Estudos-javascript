e22d94673ee0f347a714978fb4a16b0f0f46cd3a
Douglas Martins <douglas.pac@gmail.com>
2017-06-10 12:28:43 -0300

    Aula04

--- javascript-avancado/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado/client/js/app/controllers/NegociacaoController.js
@@ -5,20 +5,27 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(new Date(...
-                                        this._inputData.value
-                                        .split('-')
-                                        .map((item, indice) => item - indice % 2)),
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(DateHelper.textoParaData(this._inputData.value),
                                         this._inputQuantidade.value,
                                         this._inputValor.value
         );
-
-        console.log(negociacao.data)
-
+    }
+    _limpaFormulario(){
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
     }
 }
