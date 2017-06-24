643a28d0c6865cd915bcb263901dce1994f8b505
Douglas Martins <douglas.pac@gmail.com>
2017-06-24 19:09:59 -0300

    React S02E03

--- react/instalura/instalura/src/componentes/Timeline.js
+++ react/instalura/instalura/src/componentes/Timeline.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import Foto from './Foto';
+import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
+
+    constructor() {
+        super();
+        this.state = { fotos: [] };
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+            .then(response => response.json())
+            .then(fotos => {
+                this.setState({fotos: fotos}); 
+            });
+    }
+
     render() {
         return (
+
             <div className="fotos container">
-                <Foto />
-                <Foto />
+                {
+                    this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto}/>)
+                }
             </div>
         );
     }
